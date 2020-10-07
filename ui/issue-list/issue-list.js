b8a66ff0e02c1be593ac448cccadadc65287a91c
Fernando Daciuk <f.daciuk@gmail.com>
2020-10-07 18:10:09 -0300

    Add issues and link for next live

--- ui/issue-list/issue-list.js
+++ ui/issue-list/issue-list.js
@@ -1,18 +1,57 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import { TextBox, IssueCard, EmptyIssuesIcon, media } from 'ui'
 import { issueList } from 'resources/content'
 
+function normalizeRepo (repo) {
+  return repo.replace(/^.+\/(.+\/.+)$/, '$1')
+}
+
 export const IssueList = () => {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    async function getIssues () {
+      const repos = await Promise.all(issueList.repos.map(repo => fetch(`https://api.github.com/repos/${repo}/issues`)))
+
+      for (const repo of repos) {
+        const issues = await repo.json()
+        const issuesWithValidLabel = issues.filter(issue =>
+          issue.labels.find(label => label.name === 'hacktoberfest-lukin') &&
+          issue.state === 'open'
+        )
+
+        for (const issue of issuesWithValidLabel) {
+          const resultRepo = await fetch(issue.repository_url)
+          const stars = await resultRepo.json()
+          const repo = normalizeRepo(issue.repository_url)
+
+          const normalizedIssue = {
+            id: issue.node_id,
+            repo,
+            title: issue.title,
+            stars: stars.stargazers_count,
+            url: `https://github.com/${repo}/issues/${issue.number}`,
+          }
+
+          setCards(cards => cards.concat(normalizedIssue))
+        }
+      }
+    }
+
+    getIssues()
+  }, [])
+
   return (
     <Wrapper id='contribua'>
       <TextBox title={issueList.title} text={issueList.text} />
       <List>
-        {issueList.length && issueList.items.map(({ id, ...card }) => (
-          <IssueCard key={id} {...card} />
+        {cards.length && cards.map(({ id, ...card }, index) => (
+          <IssueCard key={id} position={index + 1} {...card} />
         ))}
       </List>
-      {!issueList.length &&
+      {!cards.length &&
         <Empty>
           <EmptyIssuesIcon />
           <Text>Em breve as issues estarão disponíveis aqui.</Text>
